9e23770b49c88f7780850ed2ab26769b8ec77456
Anthony Hansord <tonyhansorddev@gmail.com>
2024-06-15 13:58:17 +1000

    Connected to mongodb

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import crew from './routes/crew.js'
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use('/crew', crew)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
